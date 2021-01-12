b0dc7344005fbfbf0298be561039c51cbde517c3
Johan Castillo <jcjohan2707@gmail.com>
2021-01-12 13:15:41 -0400

    Table Component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div className="col-md-4">
+            <nav className="nav flex-column">
+                <a className="nav-link active" aria-current="page" href="#">Active</a>
+                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="#">Link</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Menu
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Menu from '../components/Menu'
+import Table from '../components/Table'
 
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <div className="container-fluid">
+                <div className="row">
+                   <Menu />
+                   <div className="col-md-12">
+                       <Table />
+                   </div>
+                </div>
+            </div>
         </>
     )
 }
