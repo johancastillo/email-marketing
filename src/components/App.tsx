ed11d855fdf2669b9e43f3ddf47994d3cd28d659
Johan Castillo <jcjohan2707@gmail.com>
2021-01-12 13:01:49 -0400

    Route manage with react-router-dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
+import Clients from '../pages/Clients';
+//Page
+import Home from '../pages/Home';
+import PageNotFound from '../pages/PageNotFound';
+import Providers from '../pages/Providers';
 
 function App() {
   return (
     <>
-      <h1>App Hello World</h1>
+      <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link to="/providers">Providers</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/clients" component={Clients} />
+        <Route exact path="/providers" component={Providers} />
+        <Route component={PageNotFound} />
+      </Switch>
+
+      </Router>
     </>
   );
 }
